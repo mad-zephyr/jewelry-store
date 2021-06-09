fe425404ab1c7c0393700185f08526c30f62563c
mad-zephyr <72764693+mad-zephyr@users.noreply.github.com>
2021-06-09 18:04:52 +0300

    commit message

--- app.js
+++ app.js
@@ -1,21 +1,81 @@
+require('dotenv').config()
+
+
 const express = require('express')
 const app = express()
 const path = require('path')
 const port = 3000
 
+const Prismic = require('@prismicio/client')
+const PrismicDOM = require('prismic-dom')
+
+const initApi = req => {
+
+  return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    req
+  });
+}
+
+
+// Link Resolver
+const  handleLinkResolver = doc => {
+
+  // Define the url depending on the document type
+  // if (doc.type === 'page') {
+  //   return '/page/' + doc.uid;
+  // } else if (doc.type === 'blog_post') {
+  //   return '/blog/' + doc.uid;
+  // }
+
+  // Default to homepage
+  return '/';
+}
+
+app.use((req, res, next) => {
+  res.locals.ctx = {
+    endpoint: process.env.PRISMIC_ENDPOINT,
+    linkResolver: handleLinkResolver
+  }
+  res.locals.PrismicDOM = PrismicDOM;
+  next()
+})
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-  res.render('index', {
-    meta: {
-      data: {
-        title: 'Floema',
-        description: 'Metadata'
-      }
-    }
-  })
+  res.render('pages/home')
 })
+
+app.get('/about', (req, res) => {
+  initApi(req).then(api =>{
+    api.query([
+      Prismic.Predicates.any('document.type', ['meta', 'about'])
+    ]).then(response => {
+
+      const { results } = response
+      const [ about, meta ] = results
+
+      console.log(about, meta)
+
+      res.render('pages/about', {
+        about,
+        meta
+      })
+    })
+  })
+
+})
+
+app.get('/collections', (req, res) => {
+  res.render('pages/collections')
+})
+
+app.get('/detail/:uid', (req, res) => {
+  res.render('pages/detail')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)

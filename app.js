6f21ccf8505710cc06966b38b1ad1c525242981d
Mad.Zephyr <v21400@gmail.com>
2021-06-11 00:55:20 +0300

    working on detail pug page 4/7 37m

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 
-
 const express = require('express')
+const errorhandler = require('errorhandler')
+
 const app = express()
 const path = require('path')
 const port = 3000
@@ -10,34 +11,25 @@ const Prismic = require('@prismicio/client')
 const PrismicDOM = require('prismic-dom')
 
 const initApi = req => {
-
   return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
     accessToken: process.env.PRISMIC_ACCESS_TOKEN,
     req
-  });
+  })
 }
-
 
 // Link Resolver
-const  handleLinkResolver = doc => {
-
-  // Define the url depending on the document type
-  // if (doc.type === 'page') {
-  //   return '/page/' + doc.uid;
-  // } else if (doc.type === 'blog_post') {
-  //   return '/blog/' + doc.uid;
-  // }
-
-  // Default to homepage
-  return '/';
+const handleLinkResolver = doc => {
+  return '/'
 }
+
+app.use(errorhandler())
 
 app.use((req, res, next) => {
   res.locals.ctx = {
     endpoint: process.env.PRISMIC_ENDPOINT,
     linkResolver: handleLinkResolver
   }
-  res.locals.PrismicDOM = PrismicDOM;
+  res.locals.PrismicDOM = PrismicDOM
   next()
 })
 
@@ -48,34 +40,35 @@ app.get('/', (req, res) => {
   res.render('pages/home')
 })
 
-app.get('/about', (req, res) => {
-  initApi(req).then(api =>{
-    api.query([
-      Prismic.Predicates.any('document.type', ['meta', 'about'])
-    ]).then(response => {
+app.get('/about', async (req, res) => {
+  const api = await initApi(req)
+  const about = await api.getSingle('about')
+  const meta = await api.getSingle('meta')
 
-      const { results } = response
-      const [ about, meta ] = results
-
-      console.log(about, meta)
-
-      res.render('pages/about', {
-        about,
-        meta
-      })
-    })
+  res.render('pages/about', {
+    about,
+    meta
   })
-
 })
 
 app.get('/collections', (req, res) => {
   res.render('pages/collections')
 })
 
-app.get('/detail/:uid', (req, res) => {
-  res.render('pages/detail')
-})
+app.get('/detail/:uid', async (req, res) => {
+  const api = await initApi(req)
+  const meta = await api.getSingle('meta')
+  const product = await api.getByUID('product', req.params.uid, {
+    fetchLinks: 'collection.title'
+  })
 
+  console.log(product)
+
+  res.render('pages/detail', {
+    meta,
+    product
+  })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
